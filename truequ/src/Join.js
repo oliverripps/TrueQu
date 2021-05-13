85e8ec3e37a1805e308c4dadfd928358a6609194
Oliver Ripps <oliver.ripps@com>
2021-05-13 16:25:33 -0400

    added in code input for parties

--- truequ/src/Join.js
+++ truequ/src/Join.js
@@ -1,10 +1,15 @@
+import React from 'react';
 import Home from './Home.js';
+import ReactCodeInput from 'react-code-input';
 
 function Join() {
   return (
     <div className="App">
       <Home />
-      Join a party!
+      Enter a party code
+      <div>
+        <ReactCodeInput type="text" fields={4} />
+      </div>
     </div>
   );
 }
