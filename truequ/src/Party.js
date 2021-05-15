7fbd6e6d01541669021f3b415f47e72261e41183
Oliver Ripps <oliver.ripps@com>
2021-05-15 17:52:52 -0400

    added user and party redirection

--- truequ/src/Party.js
+++ truequ/src/Party.js
@@ -1,20 +1,59 @@
 import Home from './Home.js';
 import firebase from 'firebase';
 import firestore from './Firestore';
+import { useState } from 'react';
 
 function Party(props) {
+  const [firstSession, setFirstSession] = useState(true);
   let partypath = window.location.pathname.slice(7);
   const db = firebase.firestore();
-  const partyRef = db.collection('allParties').doc(partypath).set({});
-  const activePartyRef = db.collection('activeParties').doc(partypath).set({
-    partyCode: partypath,
-    queue: [],
+  const [exists, setExists] = useState();
+  const partyRef = db.collection('allParties').doc(partypath);
+  partyRef.get().then((docSnapshot) => {
+    if (docSnapshot.exists) {
+      setExists(true);
+    } else {
+      setExists(false);
+    }
   });
+  if (exists != null && firstSession) {
+    console.log(exists);
+    let once = true;
+    if (exists) {
+      db.collection('activeParties')
+        .doc(partypath)
+        .update({
+          users: firebase.firestore.FieldValue.increment(0.5),
+        })
+        .then(console.log('added user'));
+      once = false;
+    } else {
+      partyRef.set({});
+      db.collection('activeParties').doc(partypath).set({
+        partyCode: partypath,
+        queue: [],
+        users: 1,
+      });
+    }
+    setFirstSession(false);
+  }
+  var SpotifyWebApi = require('spotify-web-api-node');
+
+  /* var spotifyApi = new SpotifyWebApi({});
+  spotifyApi.setAccessToken('<your_access_token>');
+  spotifyApi.searchTracks('Love').then(
+    function (data) {
+      console.log('Search by "Love"', data.body);
+    },
+    function (err) {
+      console.error(err);
+    }
+  ); */
   return (
     <div className="App">
       {' '}
       <Home />
-      {partypath}
+      Party Code: <b>{partypath} </b>
     </div>
   );
 }
